c62db2c1ce3fda90622a51a081103deedaedf421
John Tao <zytao24@gmail.com>
2025-11-17 18:58:38 +0800

    refactor: claude's implementation

--- Domain/Enums/Direction.cs
+++ Domain/Enums/Direction.cs
@@ -15,17 +15,44 @@ public enum Direction
 
 internal static class EnumHelper
 {
-  /*
-   * TODO: function 1
-   * pass in generic enum type
-   * return an array of enum type values
-   * option to include None (zero value enum), defaults exclude
-   * */
+    /// <summary>
+    /// Returns a HashSet of all values for the specified enum type.
+    /// </summary>
+    /// <typeparam name="T">The enum type</typeparam>
+    /// <param name="includeNone">Whether to include the zero value (None). Defaults to false.</param>
+    /// <returns>HashSet of enum values</returns>
+    public static HashSet<T> GetValues<T>(bool includeNone = false) where T : struct, Enum
+    {
+        var values = Enum.GetValues<T>();
 
-  /*
-   * TODO: function 2
-   * pass in generic enum type
-   * return an array of enum string literal
-   * option to include None (zero value enum), defaults exclude
-   * */
+        if (includeNone)
+        {
+            return new HashSet<T>(values);
+        }
+
+        // Filter out the zero value (typically "None")
+        return values.Where(v => Convert.ToInt32(v) != 0).ToHashSet();
+    }
+
+    /// <summary>
+    /// Returns a HashSet of all string names for the specified enum type.
+    /// </summary>
+    /// <typeparam name="T">The enum type</typeparam>
+    /// <param name="includeNone">Whether to include the zero value name (None). Defaults to false.</param>
+    /// <returns>HashSet of enum string literals</returns>
+    public static HashSet<string> GetNames<T>(bool includeNone = false) where T : struct, Enum
+    {
+        var names = Enum.GetNames<T>();
+
+        if (includeNone)
+        {
+            return new HashSet<string>(names);
+        }
+
+        // Filter out the name corresponding to zero value
+        var values = Enum.GetValues<T>();
+        return names
+            .Where((name, index) => Convert.ToInt32(values.GetValue(index)!) != 0)
+            .ToHashSet();
+    }
 }
