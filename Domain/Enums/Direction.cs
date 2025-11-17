732c56faab6c530282082d2b2424ee4bb4233b7c
John Tao <zytao24@gmail.com>
2025-11-17 18:57:45 +0800

    refactor: claude's work. refer to following prompt
    Please help refactor /Pages/Home.razor
    Identify redundant or duplicate code. Make it DRY

--- Domain/Enums/Direction.cs
+++ Domain/Enums/Direction.cs
@@ -12,3 +12,20 @@ public enum Direction
     Left,
     Right
 }
+
+internal static class EnumHelper
+{
+  /*
+   * TODO: function 1
+   * pass in generic enum type
+   * return an array of enum type values
+   * option to include None (zero value enum), defaults exclude
+   * */
+
+  /*
+   * TODO: function 2
+   * pass in generic enum type
+   * return an array of enum string literal
+   * option to include None (zero value enum), defaults exclude
+   * */
+}
